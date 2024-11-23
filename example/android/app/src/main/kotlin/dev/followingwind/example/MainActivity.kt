49018ba6f4270dfc68243fa9d59239611cbbd4f5
Kelvin Bulwinkel <kelvin@bulwinkel.com>
2024-11-23 13:52:43 +1000

    Initial commit

---
+++ example/android/app/src/main/kotlin/dev/followingwind/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.followingwind.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/following_wind.dart
@@ -0,0 +1,7 @@
+library following_wind;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
